8364e02ff6ad25ffd9735831bd0a595d0f515f3b
coam <zyf@iirii.com>
2025-05-29 13:38:45 +0800

    kubespray

--- bin/kubespray.sh
+++ bin/kubespray.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -xe
+set -e
 
 sc_dir="$(
   cd "$(dirname "$0")" >/dev/null 2>&1 || exit
@@ -8,6 +8,7 @@ sc_dir="$(
 
 rs_path=${sc_dir/kubespray*/kubespray}
 source $rs_path/bin/libs/headers.sh
+source $rs_path/bin/libs/others.sh
 
 Case=${1:-"test"}
 
@@ -22,16 +23,15 @@ help)
 test)
   echo "nothing to do: take it easy..."
   ;;
-init)
-  caller apt install python3-pip
-  ;;
 codes)
   caller git fetch --tags origin
   caller git checkout rebuild
   caller git merge $(git describe --tags $(git rev-list --tags --max-count=1))
   # git checkout tags/v2.28.0
-
-  caller pip3 install -r requirements.txt
+  ;;
+init)
+    caller apt install python3-pip
+    caller pip3 install -r requirements.txt
   ;;
 reconfig)
   #caller rm -rf inventory/mycluster
@@ -39,6 +39,42 @@ reconfig)
   #caller rsync -H -avP --delete --exclude=conf/{cros_headers.conf,log_format.conf,proxy_headers.conf,ssl/,luafile/} --filter='protect conf/ssl' inventory/sample inventory/mycluster
   caller rsync -H -avP --delete --filter='P hosts.yaml' inventory/sample/. inventory/mycluster
 #  python3 contrib/inventory_builder/inventory.py {{ lookup('ansible.builtin.env', 'DEPLOY_SERVERS') | split(',') | join(' ') }}
+
+#  caller sed -i "s/^minimal_node_memory_mb: 1024/minimal_node_memory_mb: 800/g" roles/kubernetes/preinstall/defaults/main.yml
+#  caller sed -i "s/^minimal_master_memory_mb: 1500/minimal_master_memory_mb: 800/g" roles/kubernetes/preinstall/defaults/main.yml
+
+#  caller sed -i "s/^kube_proxy_mode: ipvs/kube_proxy_mode: iptables/g" inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
+#  caller sed -i "s/^# supplementary_addresses_in_ssl_keys:.*/supplementary_addresses_in_ssl_keys: [ 1.cos.iirii.com ]/g" inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
+  caller sed -i "s/^enable_nodelocaldns: true/enable_nodelocaldns: false/g" inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
+
+    echo 'kube_apiserver_node_port_range: "30000-39999"' >> inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
+
+#  caller sed -i "s/^# dashboard_enabled: false/dashboard_enabled: true/g" inventory/mycluster/group_vars/k8s_cluster/addons.yml
+  caller sed -i "s/^local_volume_provisioner_enabled: false/local_volume_provisioner_enabled: true/g" inventory/mycluster/group_vars/k8s_cluster/addons.yml
+  caller sed -i "s/^cert_manager_enabled: false/cert_manager_enabled: true/g" inventory/mycluster/group_vars/k8s_cluster/addons.yml
+  caller sed -i "s/^metallb_enabled: false/metallb_enabled: true/g" inventory/mycluster/group_vars/k8s_cluster/addons.yml
+  caller sed -i "s/^argocd_enabled: false/argocd_enabled: true/g" inventory/mycluster/group_vars/k8s_cluster/addons.yml
+  caller sed -i "s/^# argocd_admin_password: \"password\"/argocd_admin_password: \"password\"/g" inventory/mycluster/group_vars/k8s_cluster/addons.yml
+  caller sed -i "s/^helm_enabled: false/helm_enabled: true/g" inventory/mycluster/group_vars/k8s_cluster/addons.yml
+#  caller sed -i "s/^krew_enabled: false/krew_enabled: true/g" inventory/mycluster/group_vars/k8s_cluster/addons.yml
+  ;;
+configs)
+    for server in root@15.zsc.iirii.com:22 root@16.zsc.iirii.com:22 root@17.zsc.iirii.com:22 root@18.zsc.iirii.com:22; do
+        parse_iirii_server $server ssh_user server_host server_port server_target server_path
+        caller ssh -o StrictHostKeyChecking=no $ssh_user@$server_host -p $server_port "sed -i '/server-[0-9]\+\.cluster\.local/d' /etc/hosts"
+        caller ssh -o StrictHostKeyChecking=no $ssh_user@$server_host -p $server_port "cat >> /etc/hosts <<'EOF'
+10.100.0.105 server-15.cluster.local server-15
+10.100.0.106 server-16.cluster.local server-16
+10.100.0.107 server-17.cluster.local server-17
+10.100.0.108 server-18.cluster.local server-18
+EOF"
+        caller ssh -o StrictHostKeyChecking=no $ssh_user@$server_host -p $server_port "cat /etc/hosts"
+    done
+
+    caller ssh -o StrictHostKeyChecking=no root@15.zsc.iirii.com -p 22 "echo server-15 > /etc/hostname && cat /etc/hostname && hostname server-15 && hostname"
+    caller ssh -o StrictHostKeyChecking=no root@16.zsc.iirii.com -p 22 "echo server-16 > /etc/hostname && cat /etc/hostname && hostname server-16 && hostname"
+    caller ssh -o StrictHostKeyChecking=no root@17.zsc.iirii.com -p 22 "echo server-17 > /etc/hostname && cat /etc/hostname && hostname server-17 && hostname"
+    caller ssh -o StrictHostKeyChecking=no root@18.zsc.iirii.com -p 22 "echo server-18 > /etc/hostname && cat /etc/hostname && hostname server-18 && hostname"
   ;;
 *)
   echo "[参数命令不合法]case: $Case [test,cluster,k8s,kubesphere,other]"
